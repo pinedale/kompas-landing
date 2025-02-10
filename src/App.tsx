cf92c9d32bbfe28c78bde28131c1d7501c4d1298
admin <admin@admins-MacBook-Pro.local>
2025-02-10 16:38:34 -0500

    kompas comming soon

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import kompasLogo from './assets/kompas.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='logo-container'>
+        <a href="https://react.dev" target="_blank">
+          <img src={kompasLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Algo nuevo viene en camino</h1>
+      
+      <p className="read-the-docs">
+        Pronto podrás explorar Kompas
+      </p>
+    </>
+  )
+}
+
+export default App
